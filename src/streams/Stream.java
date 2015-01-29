7e1a4e7db4b8b06ae88d319a3aa8099429f06baf
Adekwatny <adekwatny1@gmail.com>
2015-01-29 16:05:53 +0100

    File Byte Stream program

--- src/streams/Stream.java
+++ src/streams/Stream.java
@@ -1,10 +1,39 @@
 package streams;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Stream {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws IOException 
+	{
+		FileInputStream in = null;
+		FileOutputStream out = null;
+		
+		try
+		{
+			in = new FileInputStream("ttx.txt");
+			out = new FileOutputStream("tttx.txt");
+			
+			int i;
+			while((i = in.read()) != -1)
+			{
+				out.write(i);
+			}
+		}
+		finally
+		{
+			if (in != null)
+			{
+				in.close();
+				
+			}
+			if (out != null)
+			{
+				out.close();
+			}
+		}
 	}
 
 }
